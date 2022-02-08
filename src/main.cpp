1e211c6cc5e8d09c7d7016d5470f725f8c2efa05
PoommipatWat <poommipat_wat@cmu.ac.th>
2022-02-08 23:31:41 +0700

    "08/02/2565"

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,22 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+
+#include <iostream>
+#include <string>
+#include <windows.h>
+#include <cstdlib>
+#include <ctime>
+
 #include "poom.h"
 #include "meepooh.h"
 #include "non.h"
 #include "north.h"
-#include <SFML/Graphics.hpp>
-#include <SFML/Audio.hpp>
-#include <SFML/Network.hpp>
-#include <string>
-#include <iostream>
-#include <windows.h>
-
 
 int main()
 {
+    srand(time(0));
+    
 	sf::RenderWindow window(sf::VideoMode(1920, 1080), "REAIxProcom : Survive escape from atlantis", sf::Style::Fullscreen);
 
     while (window.isOpen())
