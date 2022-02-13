a457ae0fb91227f377ebe5c3238f68307be6b4d5
Thunyatorn Kongmoung <Thunyatorn1125@gmail.com>
2022-02-13 21:07:11 +0700

    add new

---
+++ src/north.h
@@ -0,0 +1,17 @@
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Network.hpp>
+
+// import library พื้นฐาน
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <windows.h>
+
+// import class ของสมาชิกแต่ละคน
+#include "meepooh.h"
+#include "non.h"
+#include "north.h"
+#include "poom.h"
