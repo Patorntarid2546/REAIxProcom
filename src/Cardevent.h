e7cee68dd9995cc40053586eb68effabc7bc9527
Nonlnwza007 <waritmanokaew@gmail.com>
2022-02-24 13:53:39 +0700

    "24/2/2022"

--- src/Cardevent.h
+++ src/Cardevent.h
@@ -1,29 +1,26 @@
-// #pragma once
+#pragma once
 
-// #include <SFML/Audio.hpp>
-// #include <SFML/Graphics.hpp>
-// #include <SFML/Network.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Network.hpp>
 
-// #include "Boardgame.h"
-// #include "Dolphin.h"
-// #include "Shark.h"
+#include "Boardgame.h"
+#include "Dolphin.h"
+#include "Shark.h"
 
-// #include <iostream>
-// #include <string>
-// #include <vector>
+#include <iostream>
+#include <string>
+#include <vector>
 
-// using namespace std;
+using namespace std;
 
-// class Cardevent
-// {
-// public:
-// 	vector<Dolphin>& dolphin();
-// 	vector<Shark>& shark();
-
-// 	Cardevent(vector<Dolphin>& dolphin, vector<Shark>& shark);
-// 	void PlusDolphin(vector<Dolphin>&);
-// 	void PlusShark(vector<Shark>&);
-// 	void MoveDolphin(vector<Dolphin>&);
-// 	void MoveShark(vector<Shark>&);
-// 	void DeleteShark(vector<Shark>&);
-// };
+class Cardevent
+{
+public:
+	Cardevent(bool, vector<Dolphin>& dolphin, vector<Shark>& shark, vector<Boardgame>&);
+	void PlusDolphin(vector<Dolphin>&, vector<Boardgame>&);
+	void PlusShark(vector<Shark>&, vector<Boardgame>&);
+	void MoveDolphin(vector<Dolphin>&, vector<Boardgame>&);
+	void MoveShark(vector<Shark>&, vector<Boardgame>&);
+	void DeleteShark(vector<Shark>&);
+};
